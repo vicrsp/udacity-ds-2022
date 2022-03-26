35e44fe71cf1974d327f98aaccfc944f71f2cda1
Victor Ruela <victorspruela@gmail.com>
2022-03-26 11:58:27 -0300

    - reviewing train script

--- src/project_02/models/train_classifier.py
+++ src/project_02/models/train_classifier.py
@@ -9,18 +9,33 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import VarianceThreshold, SelectFromModel
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
+from sklearn.decomposition import PCA
+from sklearn.cluster import FeatureAgglomeration
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.neighbors import KNeighborsClassifier
 
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 
+
+class DenseTransformer(TransformerMixin, BaseEstimator):
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, y=None, **fit_params):
+        return X.todense()
+   
 
 def load_data(database_filepath):
     engine = create_engine(f'sqlite:///{database_filepath}')
@@ -35,63 +50,67 @@ def load_data(database_filepath):
        'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
        'other_weather', 'direct_report']
 
-    X = df.message.values
-    Y = df[target_columns].values
-    
+    X = df.message.astype(str).values
+    Y = df[target_columns].astype(int).values
     return X, Y, target_columns
 
 def tokenize(text):
     # Normalize case and remove punctuation
-    text = re.sub(r"[^a-zA-Z0-9]", " ", text).lower().strip()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
     # Tokenize text
     tokens = word_tokenize(text)
+    
+    # Remove stop words    
+    stop_words = stopwords.words("english")
+    filtered_words = [word for word in tokens if word not in stop_words]
+    
     # Lemmatize and clean text
     lemmatizer = WordNetLemmatizer()
     clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok)
+    for tok in filtered_words:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-    # Remove stop words    
-    stop_words = stopwords.words("english")
-    filtered_words = [word for word in clean_tokens if word not in stop_words]
-    return filtered_words
+    
+    return clean_tokens
 
 
 def build_model():
     # instantiate transformers and classifiers
     vect = CountVectorizer(tokenizer=tokenize)
     tfidf = TfidfTransformer()
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(random_state=0)
+    vt = VarianceThreshold()
     
     # create the pipeline
     pipeline = Pipeline([
                 ('text_pipeline', Pipeline([
-                    ('vect', CountVectorizer(tokenizer=tokenize)),
-                    ('tfidf', TfidfTransformer())
+                    ('vect', vect),
+                    ('tfidf', tfidf)
                 ])),
-                ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                ('variance_threshold', vt),
+                ('clf', MultiOutputClassifier(clf, n_jobs=-1))
                 ])
     
+ 
     parameters = {
-        'text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
-        'text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
-        'text_pipeline__vect__max_features': (None, 5000, 10000),
         'clf__estimator__n_estimators': [50, 100, 200],
-        'clf__estimator__min_samples_split': [2, 3, 4]
+        'clf__estimator__min_samples_split': [2, 5, 10],
+        'clf__estimator__max_depth': [2, 5, None],
+        'text_pipeline__vect__ngram_range': [(1, 1), (1, 2)],
     }
-
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    cv = RandomizedSearchCV(pipeline, parameters, n_iter=10, verbose=3, random_state=0)
     return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    y_pred_cv = model.best_estimator_.predict(X_test)
+    y_pred = model.predict(X_test)
     for i, value in enumerate(category_names):    
         print(f'Category: {value}')
-        print(classification_report(Y_test[i], y_pred_cv[i]))
+        print(classification_report(Y_test[i], y_pred[i]))
 
 
 def save_model(model, model_filepath):
-    joblib.dump(model.best_estimator_, model_filepath)
+    joblib.dump(model, model_filepath)
 
 def main():
     if len(sys.argv) == 3:
@@ -107,10 +126,10 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model.best_estimator_, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        save_model(model.best_estimator_, model_filepath)
 
         print('Trained model saved!')
 
